5ad227adcebe6c7aa4aa513bf4bd8761116c27e1
root <mikesware@gmail.com>
2025-03-05 12:03:38 -0500

    added setstaticip.ps1 and setwallpaper.ps1

---
+++ SetStaticip.ps1
@@ -0,0 +1,22 @@
+﻿Get-NetAdapter -Name Ethernet
+# Define the network interface name
+$InterfaceAlias = "Ethernet"
+
+# Define the static IP address, subnet mask, and default gateway
+$IPAddress = "192.168.0.13"
+$PrefixLength = 24
+$DefaultGateway = "192.168.0.2"
+
+# Define the DNS server addresses
+
+$DNSServer1 = "8.8.8.8"
+$DNSServer2 = "8.8.4.4"
+$DNSServer3 = "192.168.0.16"
+
+$DNSServer4 = "192.168.0.19"
+
+# Set the static IP address
+New-NetIPAddress -InterfaceAlias $InterfaceAlias -IPAddress $IPAddress -PrefixLength $PrefixLength -DefaultGateway $DefaultGateway
+
+# Set the DNS server addresses
+Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses ($DNSServer3, $DNSServer4,$DNSServer1, $DNSServer2)
---
+++ SetWallpaper.ps1
@@ -0,0 +1,10 @@
+﻿
+#Enter-PSSession -ComputerName <RemoteComputerName> -Credential <Domain\Username>
+$wfile="C:\DATA\MW.JPG"
+$wfile="C:\DATA\Mjr.jpg"
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper -Value $wfile
+    Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name WallpaperStyle -Value 2
+    Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name TileWallpaper -Value 0
+    rundll32.exe user32.dll, UpdatePerUserSystemParameters 1 , TRUE
+    Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper 
+    
